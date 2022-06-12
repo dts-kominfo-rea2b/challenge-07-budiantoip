a7555de71f73e1b3b64541824c811c4fa7095389
Budianto IP <budiantoip@gmail.com>
2022-06-12 22:21:21 +0700

    work on promise

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let total = 0;
+  
+  const theatherIXX = await promiseTheaterIXX().then(value => {
+    value.forEach(item => {
+      if (item.hasil == emosi) {
+        total++;
+      }
+    });
+  }).catch ( error => {
+    console.log("error: ", error);
+  });
+
+  const theatherVGC = await promiseTheaterVGC()
+    .then((value) => {
+      value.forEach((item) => {
+        if (item.hasil == emosi) {
+          total++;
+        }
+      });
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+    });
+  
+  return total;
+};
 
 module.exports = {
   promiseOutput,
